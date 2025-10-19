b0fff846ad8904ae57351a0a94ceea8aeb2d285a
Infinity <igor74853@yandex.ru>
2025-10-19 14:22:18 +0500

    feat: add PostgreSQL integration43

--- backend/src1/repository/films.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { FilmDto, FilmScheduleDto } from '../films/dto/films.dto';
-
-export interface FilmsRepository {
-  findAll(): Promise<FilmDto[]>;
-  findById(id: string): Promise<FilmScheduleDto | null>;
-}
--- backend/src1/repository/order.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { CreateOrderDto, OrderDto } from '../order/dto/order.dto';
-
-export interface OrderRepository {
-  create(order: CreateOrderDto): Promise<OrderDto>;
-  findById(id: string): Promise<OrderDto | null>;
-  confirmOrder(id: string): Promise<OrderDto>;
-}
